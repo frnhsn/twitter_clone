563f4e783beb0574a943422fea4c9d40afd57a02
Farhan <saturnv@Farhans-MacBook-Air.local>
2021-01-22 21:59:00 +0700

    make it better 4

--- twitter_clone/urls.py
+++ twitter_clone/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.generics import TemplateView
+from django.views.generic import TemplateView
 
 from rest_framework import routers
 
